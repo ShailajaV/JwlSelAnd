60ca5b2baf916b38f7e5398556192ab336ccaf1e
ShailajaV <shailaja.v8@gmail.com>
2017-03-07 16:05:04 -0600

    logo

--- src/components/auth/PreLoginForm.js
+++ src/components/auth/PreLoginForm.js
@@ -1,15 +1,35 @@
 /* User Pre login form */
 import React, { Component } from 'react';
+import { Image } from 'react-native';
 import { connect } from 'react-redux';
 import { signIn, signUp } from '../../actions';
 import { Card, CardSection, Button } from '../common';
 import { SIGN_IN, SIGN_UP } from '../../actions/constants';
 
 class PreLoginForm extends Component {
+  onLogInButton() {
+      this.props.signIn();
+    }
+
+  onSignUpButton() {
+    this.props.signUp();
+  }
 
   render() {
     return (
+
       <Card style={styles.containerStyle}>
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+          <CardSection>
+          <Image
+            source={require('../common/images/logo.png')}
+            style={styles.upload}
+            resizeMode={Image.resizeMode.sretch}
+          />
+          </CardSection>
         <CardSection>
           <Button onPress={this.onLogInButton.bind(this)}>
             {SIGN_IN}
@@ -25,20 +45,21 @@ class PreLoginForm extends Component {
 const styles = {
   containerStyle: {
     flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'center',
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
     alignItems: 'center',
     marginLeft: 0,
     marginRight: 0,
-    marginTop: 0
+    marginTop: 0,
+    backgroundColor: '#1abc9c'
   },
-  contentHeaderMenuLink: {
-    textDecoration: 'none',
-    color: 'white',
-    padding: 8,
+  cardUpload: {
+    padding: 150
   },
-  content: {
-    padding: '16px',
+  upload: {
+    borderRadius: 75,
+    width: 150,
+    height: 150
   }
 };
 export default connect(null, { signIn, signUp })(PreLoginForm);
