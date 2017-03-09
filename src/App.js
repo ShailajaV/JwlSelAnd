710a7c6e01a903028e60c3f17f01fdea3e8db61b
ShailajaV <shailaja.v8@gmail.com>
2017-03-08 22:24:38 -0600

    movedCSS

--- src/App.js
+++ src/App.js
@@ -1,82 +1,78 @@
 /* Welcome file which starts rendering the app */
 import React, { Component } from 'react';
-import { Provider } from 'react-redux';
+import { connect } from 'react-redux';
 import firebase from 'firebase';
 import SideMenu from 'react-native-side-menu';
-import { View } from 'react-native';
 import { Actions } from 'react-native-router-flux';
 import { MenuHeader } from './components/common';
 import Menu from './components/menu/Menu';
 import Router from './Router';
-import configureStore from './ConfigureStore';
+import { logOut } from './actions';
 
 class App extends Component {
   state = {
       isOpen: false,
-      selectedItem: 'About',
+      selectedItem: 'LoginForm',
+  };
+
+  componentWillMount() {
+    const config = {
+    apiKey: 'AIzaSyDFmxMQ6Fp55hh6RQiaUuIHsUYDlTKySZc',
+    authDomain: 'jewellery-ece6f.firebaseapp.com',
+    databaseURL: 'https://jewellery-ece6f.firebaseio.com',
+    storageBucket: 'jewellery-ece6f.appspot.com',
+    messagingSenderId: '601320078334'
     };
+  firebase.initializeApp(config);
+  }
+  onMenuItemSelected = (item) => {
+    this.setState({
+      isOpen: false,
+      selectedItem: item,
+    });
+    this.goToScreen(item);
+  }
 
-    componentWillMount() {
-      const config = {
-      apiKey: 'AIzaSyDFmxMQ6Fp55hh6RQiaUuIHsUYDlTKySZc',
-      authDomain: 'jewellery-ece6f.firebaseapp.com',
-      databaseURL: 'https://jewellery-ece6f.firebaseio.com',
-      storageBucket: 'jewellery-ece6f.appspot.com',
-      messagingSenderId: '601320078334'
-      };
-    firebase.initializeApp(config);
+  goToScreen(route) {
+    switch (route) {
+      case 'LoginForm':
+        return Actions.logIn();
+      case 'AccountSettings':
+        return Actions.sellerProfile();
+      case 'ProductCreate':
+        return Actions.productDetails();
+      case 'ProductDetails':
+        return Actions.productsList();
+      case 'ForgotPassword':
+        return Actions.forgotPassword();
+      case 'Logout':
+        return this.props.logOut();
+      default:
+        return Actions.logIn();
     }
-    onMenuItemSelected = (item) => {
-      this.setState({
-        isOpen: false,
-        selectedItem: item,
-      });
-      this.goToScreen(item);
-    }
-
-    goToScreen(route) {
-      switch (route) {
-        case 'LoginForm':
-          return Actions.logIn();
-        case 'AccountSettings':
-          return Actions.sellerProfile();
-        case 'ProductCreate':
-          return Actions.productDetails();
-        case 'ProductDetails':
-          return Actions.productsList();
-        case 'ForgotPassword':
-          return Actions.forgotPassword();
-        default:
-          return Actions.logIn();
-      }
-    }
-    toggle() {
-      this.setState({
-        isOpen: !this.state.isOpen,
-      });
-    }
-    updateMenuState(isOpen) {
-      this.setState({ isOpen });
-    }
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen,
+    });
+  }
+  updateMenuState(isOpen) {
+    this.setState({ isOpen });
+  }
 
   render() {
-    const store = configureStore();
-const menu = <Menu onItemSelected={this.onMenuItemSelected} />;
+    const menu = <Menu onItemSelected={this.onMenuItemSelected} />;
     return (
-        <SideMenu
-          menu={menu}
-          isOpen={this.state.isOpen}
-          onChange={(isOpen) => this.updateMenuState(isOpen)}
-        >
-        <View style={{ backgroundColor: '#1abc9c' }}>
-          <MenuHeader headerText='Toggle' onPress={() => this.toggle()} />
-        </View>
-        <Provider store={store}>
-         <Router />
-        </Provider>
-        </SideMenu>
+      <SideMenu
+        menu={menu}
+        isOpen={this.state.isOpen}
+        onChange={(isOpen) => this.updateMenuState(isOpen)}
+      >
+        <MenuHeader headerText='Toggle' onPress={() => this.toggle()} />
+        <Router />
+      </SideMenu>
     );
   }
 }
 
-export default App;
+export default connect(null, { logOut })(App);
