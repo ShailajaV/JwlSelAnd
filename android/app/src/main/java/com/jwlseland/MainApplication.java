8f54a78a5d86cd1af679d8da1a5b828e531b6b03
ShailajaV <shailaja.v8@gmail.com>
2017-04-17 17:37:11 -0500

    header_logo

--- android/app/src/main/java/com/jwlseland/MainApplication.java
+++ android/app/src/main/java/com/jwlseland/MainApplication.java
@@ -6,8 +6,6 @@ import android.util.Log;
 import com.facebook.react.ReactApplication;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.imagepicker.ImagePickerPackage;
-import com.RNFetchBlob.RNFetchBlobPackage;
-import com.imagepicker.ImagePickerPackage;
 import com.rnfs.RNFSPackage;
 import com.RNFetchBlob.RNFetchBlobPackage;
 import com.facebook.react.ReactInstanceManager;
@@ -33,8 +31,8 @@ public class MainApplication extends Application implements ReactApplication {
           new MainReactPackage(),
             new VectorIconsPackage(),
             new ImagePickerPackage(),
-            new RNFetchBlobPackage(),
-            new RNFSPackage()
+			new RNFetchBlobPackage(),
+			new RNFSPackage()
       );
     }
   };
